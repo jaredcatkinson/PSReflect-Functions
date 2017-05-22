bd496313939e929569ed935f0d9efaf16ab5e34e
Jared Atkinson <jared@invoke-ir.com>
2017-05-22 20:40:11 +0200

    CloseHandle and RevertToSelf

---
+++ advapi32/RevertToSelf.ps1
@@ -0,0 +1,40 @@
+﻿function RevertToSelf
+{
+    <#
+    .SYNOPSIS
+
+    The RevertToSelf function terminates the impersonation of a client application.
+
+    .DESCRIPTION
+
+    A process should call the RevertToSelf function after finishing any impersonation begun by using the DdeImpersonateClient, ImpersonateDdeClientWindow, ImpersonateLoggedOnUser, ImpersonateNamedPipeClient, ImpersonateSelf, ImpersonateAnonymousToken or SetThreadToken function.
+    
+    An RPC server that used the RpcImpersonateClient function to impersonate a client must call the RpcRevertToSelf or RpcRevertToSelfEx to end the impersonation.
+    
+    If RevertToSelf fails, your application continues to run in the context of the client, which is not appropriate. You should shut down the process if RevertToSelf fails.
+
+    .NOTES
+
+    Author: Jared Atkinson (@jaredcatkinson)
+    License: BSD 3-Clause
+    Required Dependencies: None
+    Optional Dependencies: None
+
+    (func advapi32 RevertToSelf ([bool]) @() -SetLastError)
+
+    .LINK
+
+    https://msdn.microsoft.com/en-us/library/windows/desktop/aa379317(v=vs.85).aspx
+
+    .EXAMPLE
+
+        RevertToSelf
+    #>
+
+    $SUCCESS = $Advapi32::RevertToSelf(); $LastError = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+    
+    if(-not $SUCCESS)
+    {
+        throw "RevertToSelf Error: $(([ComponentModel.Win32Exception] $LastError).Message)"
+    }
+}
---
+++ kernel32/CloseHandle.ps1
@@ -0,0 +1,68 @@
+﻿function CloseHandle
+{
+    <#
+    .SYNOPSIS
+
+    Closes an open object handle.
+
+    .DESCRIPTION
+
+    The CloseHandle function closes handles to the following objects:
+    - Access token
+    - Communications device
+    - Console input
+    - Console screen buffer
+    - Event
+    - File
+    - File mapping
+    - I/O completion port
+    - Job
+    - Mailslot
+    - Memory resource notification
+    - Mutex
+    - Named pipe
+    - Pipe
+    - Process
+    - Semaphore
+    - Thread
+    - Transaction
+    - Waitable timer
+    
+    The documentation for the functions that create these objects indicates that CloseHandle should be used when you are finished with the object, and what happens to pending operations on the object after the handle is closed. In general, CloseHandle invalidates the specified object handle, decrements the object's handle count, and performs object retention checks. After the last handle to an object is closed, the object is removed from the system. 
+
+    .PARAMETER Handle
+
+    A valid handle to an open object.
+
+    .NOTES
+
+    Author: Jared Atkinson (@jaredcatkinson)
+    License: BSD 3-Clause
+    Required Dependencies: None
+    Optional Dependencies: None
+    
+    (func kernel32 CloseHandle ([bool]) @(
+        [IntPtr] #_In_ HANDLE hObject
+    ) -SetLastError)
+
+    .LINK
+
+    https://msdn.microsoft.com/en-us/library/windows/desktop/ms724211(v=vs.85).aspx
+
+    .EXAMPLE
+    #>
+
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [IntPtr]
+        $Handle    
+    )
+    
+    $SUCCESS = $Kernel32::CloseHandle($Handle); $LastError = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+
+    if(-not $SUCCESS) 
+    {
+        Write-Debug "CloseHandle Error: $(([ComponentModel.Win32Exception] $LastError).Message)"
+    }
+}

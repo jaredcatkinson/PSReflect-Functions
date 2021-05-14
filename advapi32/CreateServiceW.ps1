970f24f48b5064e1c04ba01022a1e08394529f43
Jared Atkinson <jaredcatkinson@users.noreply.github.com>
2021-05-14 14:57:10 -0700

    Update CreateServiceW.ps1

--- advapi32/CreateServiceW.ps1
+++ advapi32/CreateServiceW.ps1
@@ -158,7 +158,7 @@
         $dwServiceType = $dwServiceType -bor $SERVICE_TYPE::$val
     }
 
-    $handle = $Advapi32::CreateServiceW($Handle, $ServiceName, $DisplayName, $dwDesiredAccess, $dwServiceType, $SERVICE_START_TYPE::$StartType, $SERVICE_ERROR::$ErrorControl, $BinaryPathName, '', [IntPtr]::Zero, '', $UserName, $Password);$LastError = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+    $handle = $Advapi32::CreateServiceW($Handle, $ServiceName, $DisplayName, $dwDesiredAccess, $dwServiceType, $SERVICE_START_TYPE::$StartType, $SERVICE_ERROR::$ErrorControl, $BinaryPathName, '', [IntPtr]::Zero, '', $NULL, $NULL);$LastError = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
 
     # if we get a non-zero handle back, everything was successful
     if ($handle -ne 0) {

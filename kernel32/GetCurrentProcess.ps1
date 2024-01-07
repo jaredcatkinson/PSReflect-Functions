c5307bbd66eaf5d58c66744363a896ff04b7d4cc
Jared Atkinson <jaredcatkinson@users.noreply.github.com>
2024-01-07 01:22:21 -0500

    Update GetCurrentProcess.ps1

--- kernel32/GetCurrentProcess.ps1
+++ kernel32/GetCurrentProcess.ps1
@@ -1,4 +1,4 @@
-function GetCurrentProcess
+﻿function GetCurrentProcess
 {
     <#
     .SYNOPSIS
@@ -7,29 +7,26 @@ function GetCurrentProcess
 
     .DESCRIPTION
 
-    Retrieves a pseudo handle for the current process.
+    A pseudo handle is a special constant, currently (HANDLE)-1, that is interpreted as the current process handle. For compatibility with future operating systems, it is best to call GetCurrentProcess instead of hard-coding this constant value. The calling process can use a pseudo handle to specify its own process whenever a process handle is required. Pseudo handles are not inherited by child processes.
+
+    This handle has the PROCESS_ALL_ACCESS access right to the process object. For more information, see Process Security and Access Rights.
 
     .NOTES
 
-    Author: Will Schroeder (@harmj0y)  
-    License: BSD 3-Clause  
-    Required Dependencies: PSReflect  
+    Author: Jared Atkinson (@jaredcatkinson)
+    License: BSD 3-Clause
+    Required Dependencies: None
     Optional Dependencies: None
 
-    (func kernel32 GetCurrentProcess ([IntPtr]) @(
-    
-    ) -EntryPoint GetCurrentProcess)
+    (func kernel32 GetCurrentProcess ([IntPtr]) @() -EntryPoint GetCurrentProcess)
 
     .LINK
-
-    https://msdn.microsoft.com/en-us/library/windows/desktop/ms683179(v=vs.85).aspx
-
-    .EXAMPLE
-
+    
+    https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentprocess
     #>
 
     [OutputType([IntPtr])]
-    Param()
+    param()
 
-    $Kernel32::GetCurrentProcess()
+    $kernel32::GetCurrentProcess()
 }
